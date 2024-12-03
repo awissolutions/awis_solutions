46523832748e1a32da2ea7ff159607299a9e4ec0
sisyphys_666 <139012668+Umer-Islam@users.noreply.github.com>
2024-12-04 02:55:01 +0500

    update text

--- src/components/WhyUs/index.tsx
+++ src/components/WhyUs/index.tsx
@@ -56,10 +56,10 @@ const WhyUs = () => {
       <div className="container">
         <div className="mb-10 text-center">
           <h2 className="text-3xl font-bold text-dark dark:text-white">
-            Why Choose Daybook’s Bookkeeping And Accounting Services?
+            Why Choose awis solutions Bookkeeping And Accounting Services?
           </h2>
           <p className="mt-4 text-base text-body-color dark:text-dark-6">
-            At Daybook, we understand the unique challenges faced by small
+            At awis solutions, we understand the unique challenges faced by small
             business owners. Our services go beyond numbers, providing value
             that moves your business forward.
           </p>
