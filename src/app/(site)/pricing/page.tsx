28e023c88e3eddc72e2e1e14fb588948b4f3923b
sisyphys_666 <139012668+Umer-Islam@users.noreply.github.com>
2024-12-06 18:54:03 +0500

    update metadata of privacy-policy

--- src/app/(site)/pricing/page.tsx
+++ src/app/(site)/pricing/page.tsx
@@ -5,8 +5,8 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title:
-    "Pricing Page | Play SaaS Starter Kit and Boilerplate for Next.js",
-  description: "This is pricing page description",
+    "Pricing Page | Awis Solutions",
+  description: "This is pricing page Awis Solutions",
 };
 
 const PricingPage = () => {
--- src/app/(site)/privacy-policy/page.tsx
+++ src/app/(site)/privacy-policy/page.tsx
@@ -1,5 +1,10 @@
 import PrivacyPolicy from '@/components/PrivacyPolicy'
 import React from 'react'
+export const metadata: Metadata = {
+  title:
+    "Privacy Policy | Awis Solutions",
+  description: "Privacy Policy page, Awis Solutions",
+};
 
 function page() {
   return (
