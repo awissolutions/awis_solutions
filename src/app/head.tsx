6fabec4f6243e516225433ee5b497cfc026a58bd
sisyphys_666 <139012668+Umer-Islam@users.noreply.github.com>
2024-12-18 19:55:03 +0500

    update titile

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,9 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>
-      awis solutions - Lorem ipsum dolor sit.
-      </title>
+      <title>awis solutions - Simplifying financial management</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="awis solutions - " />
       <link rel="icon" href="/images/favicon.ico" />
