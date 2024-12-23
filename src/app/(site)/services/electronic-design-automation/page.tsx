2c6be1f768aabba8b98bc53936dd2487d8d7adca
sisyphys_666 <139012668+Umer-Islam@users.noreply.github.com>
2024-12-23 13:41:48 +0500

    update component

--- src/app/(site)/services/electronic-design-automation/page.tsx
+++ src/app/(site)/services/electronic-design-automation/page.tsx
@@ -1,6 +1,6 @@
 import Breadcrumb from '@/components/Common/Breadcrumb'
 import Features from '@/components/services/electronic-design-automation/Features'
-import Hero from '@/components/services/DemandProcedure/Hero'
+import Hero from '@/components/services/electronic-design-automation/Hero'
 import React from 'react'
 import Benefits from '@/components/services/electronic-design-automation/Benefits'
 import Process from '@/components/services/electronic-design-automation/Process'
