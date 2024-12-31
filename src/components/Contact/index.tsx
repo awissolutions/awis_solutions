5d8f98330fd1d09122485eb31e1944a5dd208f64
sisyphys_666 <139012668+Umer-Islam@users.noreply.github.com>
2025-01-01 01:03:54 +0500

    increase rows for message

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -203,7 +203,7 @@ const Contact = () => {
               </div>
             </div>
           </div>
-          <div className="w-full px-4 lg:w-5/12 xl:w-4/12">
+          <div className="w-full px-4 lg:w-5/10 xl:w-4/12">
             <div className="wow fadeInUp rounded-lg bg-white px-8 py-10 shadow-testimonial dark:bg-dark-2 dark:shadow-none sm:px-10 sm:py-12 md:p-[60px] lg:p-10 lg:px-10 lg:py-12 2xl:p-[60px]">
               <h3 className="mb-8 text-2xl font-semibold text-dark dark:text-white md:text-[28px] md:leading-[1.42]">
                 Send us a Message
@@ -270,7 +270,7 @@ const Contact = () => {
                   <textarea
                     id="message"
                     name="message"
-                    rows={1}
+                    rows={5}
                     placeholder="type your message here"
                     className="w-full resize-none border-0 border-b border-[#f1f1f1] bg-transparent pb-3 text-dark placeholder:text-body-color/60 focus:border-primary focus:outline-none dark:border-dark-3 dark:text-white"
                     onBlur={handleBlur}
