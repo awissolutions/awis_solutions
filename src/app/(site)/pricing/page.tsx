d7c0080061058a3efdabe7bf123f906686f1eb4d
sisyphys_666 <139012668+Umer-Islam@users.noreply.github.com>
2025-01-25 11:39:04 +0500

    commented pricing page

--- src/app/(site)/pricing/page.tsx
+++ src/app/(site)/pricing/page.tsx
@@ -1,22 +1,22 @@
-import Breadcrumb from "@/components/Common/Breadcrumb";
-import Faq from "@/components/Faq";
-import Pricing from "@/components/Pricing";
-import { Metadata } from "next";
+// import Breadcrumb from "@/components/Common/Breadcrumb";
+// import Faq from "@/components/Faq";
+// import Pricing from "@/components/Pricing";
+// import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title:
-    "Pricing Page | Awis Solutions",
-  description: "This is pricing page Awis Solutions",
-};
+// export const metadata: Metadata = {
+//   title:
+//     "Pricing Page | Awis Solutions",
+//   description: "This is pricing page Awis Solutions",
+// };
 
-const PricingPage = () => {
-  return (
-    <>
-      <Breadcrumb pageName="Pricing Page" />
-      <Pricing />
-      <Faq />
-    </>
-  );
-};
+// const PricingPage = () => {
+//   return (
+//     <>
+//       <Breadcrumb pageName="Pricing Page" />
+//       <Pricing />
+//       <Faq />
+//     </>
+//   );
+// };
 
-export default PricingPage;
+// export default PricingPage;
